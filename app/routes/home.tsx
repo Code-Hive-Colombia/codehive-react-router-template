74ae5dd260a2f8047c92559ee6feb4b3a02e8a5c
nulldev <exception.nulldev@gmail.com>
2025-03-24 16:26:33 -0500

    ✨ (index-page): Upgrade index page with some examples

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,59 @@
+import { Await } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import React from "react";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CodeHive" },
+    { name: "description", content: "CodeHive Template" },
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.VALUE_FROM_EXPRESS };
+export function loader({ request, context }: Route.LoaderArgs) {
+  // Express context
+  const { EXPRESS_CONTEXT_EXAMPLE: express } = context;
+
+  // Search Params
+  const url = new URL(request.url);
+  const name = url.searchParams.get("name") ?? 'Use "?name=<string>" in url';
+
+  // Async operation
+  const getAsyncOperation = async () => {
+    await new Promise((res) => setTimeout(res, 5000));
+    return "Promised value";
+  };
+
+  const asyncValue = getAsyncOperation();
+
+  return { name, express, asyncValue };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  const { name, express, asyncValue } = loaderData;
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold text-primary">Index Page</h1>
+
+      <ul className="list-disc list-inside mt-4 font-mon">
+        <li>URL SearchParams: {name}</li>
+        <li>Express Context: {express}</li>
+        <li>
+          Async Value:{" "}
+          <React.Suspense
+            fallback={
+              <span className="animate-pulse">
+                <span className="bg-gray-300 text-gray-500 min-h-2 px-8 rounded-lg">
+                  Cargando
+                </span>
+              </span>
+            }
+          >
+            <Await resolve={asyncValue}>
+              {(value) => <span>{value}</span>}
+            </Await>
+          </React.Suspense>
+        </li>
+      </ul>
+    </main>
+  );
 }
